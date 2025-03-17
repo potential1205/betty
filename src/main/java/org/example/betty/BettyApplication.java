44930c8f3b94970f2bc360cdd13e7dcf69f9ece9
이재훈 <dlwogns1205@naver.com>
2025-03-18 00:46:47 +0900

    init: 초기 프로젝트 설정 [S12P21A609-80]

---
+++ src/main/java/org/example/betty/BettyApplication.java
@@ -0,0 +1,13 @@
+package org.example.betty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BettyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/betty/BettyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.betty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

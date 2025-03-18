f51df15b40b1cf53ff830780f943dc33b95351a6
이재훈 <dlwogns1205@naver.com>
2025-03-18 12:50:00 +0900

    feat: 테스트 API 작성 [S12P21A609-86]

--- src/main/java/org/example/betty/domain/match/controller/MatchController.java
+++ src/main/java/org/example/betty/domain/match/controller/MatchController.java
@@ -1,4 +1,13 @@
 package org.example.betty.domain.match.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MatchController {
+
+    @GetMapping("/test")
+    public String testServer() {
+        return "EC2 서버가 정상적으로 동작합니다!";
+    }
 }

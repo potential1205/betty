94122b81ee21a17d086b469810b91d0d3436ced7
이재훈 <dlwogns1205@naver.com>
2025-03-18 12:54:47 +0900

    feat: MatchController Request Mapping 설정 [S12P21A609-88]

--- src/main/java/org/example/betty/domain/match/controller/MatchController.java
+++ src/main/java/org/example/betty/domain/match/controller/MatchController.java
@@ -1,9 +1,11 @@
 package org.example.betty.domain.match.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/match")
 public class MatchController {
 
     @GetMapping("/test")

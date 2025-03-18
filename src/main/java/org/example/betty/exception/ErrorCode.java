05b93406abc284dd1751fd91f36d4b79212c5675
이재훈 <dlwogns1205@naver.com>
2025-03-18 14:54:59 +0900

    feat: 예외 처리 로직 수정 [S12P21A609-95]

--- src/main/java/org/example/betty/exception/ErrorCode.java
+++ src/main/java/org/example/betty/exception/ErrorCode.java
@@ -5,13 +5,15 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "GAME_NOT_FOUND_001", "해당하는 Game이 없습니다.");
+    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, 1001, "해당하는 Game이 없습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, 0000, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 0001, "예상치 못한 서버 오류가 발생했습니다.");
 
     private final HttpStatus status;
-    private final String code;
+    private final int code;
     private final String message;
 
-    ErrorCode(HttpStatus status, String code, String message) {
+    ErrorCode(HttpStatus status, int code, String message) {
         this.status = status;
         this.code = code;
         this.message = message;

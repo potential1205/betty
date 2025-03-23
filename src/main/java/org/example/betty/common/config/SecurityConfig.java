d56ad30e0b47a4455f0235e137afd992e2c608cc
이재훈 <dlwogns1205@naver.com>
2025-03-24 00:16:58 +0900

    feat: Swagger 오류 해결 [S12P21A609-173]

--- src/main/java/org/example/betty/common/config/SecurityConfig.java
+++ src/main/java/org/example/betty/common/config/SecurityConfig.java
@@ -25,7 +25,13 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/v1/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth -> oauth

ae84bd72e32f683dcac6085ff58d7eb47e3cb652
이재훈 <dlwogns1205@naver.com>
2025-03-24 00:05:42 +0900

    feat: Auth 도메인 개발 (로그인 기능) [S12P21A609-172]

---
+++ src/main/java/org/example/betty/common/jwt/JwtProvider.java
@@ -0,0 +1,29 @@
+package org.example.betty.common.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    private final JwtEncoder encoder;
+
+    public String createAccessToken(String subject) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(subject)
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.HOURS))
+                .build();
+
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
---
+++ src/main/java/org/example/betty/domain/auth/dto/resp/LoginResponse.java
@@ -0,0 +1,14 @@
+package org.example.betty.domain.auth.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+}

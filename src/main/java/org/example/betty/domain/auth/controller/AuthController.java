83bf41c1421995907d8d1821a5881d159730575c
이재훈 <dlwogns1205@naver.com>
2025-03-26 17:38:21 +0900

    feat: 불필요한 import 제거 [S12P21A609-225]

--- src/main/java/org/example/betty/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/betty/domain/auth/controller/AuthController.java
@@ -1,29 +1,15 @@
 package org.example.betty.domain.auth.controller;
 
-import com.nimbusds.jwt.JWTClaimsSet;
-import jakarta.servlet.http.HttpSession;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bouncycastle.jce.ECNamedCurveTable;
-import org.bouncycastle.math.ec.ECPoint;
+
 import org.example.betty.common.resp.SuccessResponse;
 import org.example.betty.domain.auth.dto.req.LoginRequest;
 import org.example.betty.domain.auth.service.AuthService;
-import org.example.betty.domain.auth.service.TokenService;
-import org.example.betty.domain.auth.service.Web3AuthService;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.web3j.crypto.Keys;
-import org.web3j.utils.Numeric;
-
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.HexFormat;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/auth")

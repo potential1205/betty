03ff30fbfbac6a27d2134e75b73d835108d5f881
이재훈 <dlwogns1205@naver.com>
2025-03-27 13:35:06 +0900

    feat: Swagger 작성 / 오류 수정[S12P11A808-243]

--- src/main/java/org/example/betty/BettyApplication.java
+++ src/main/java/org/example/betty/BettyApplication.java
@@ -1,9 +1,11 @@
 package org.example.betty;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class BettyApplication {
 

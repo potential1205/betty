5e90e8bba238f09de9ee049faac6c28a27e15c3c
이재훈 <dlwogns1205@naver.com>
2025-03-27 18:52:08 +0900

    feat: allowCredentials 옵션 변경[S12P11A808-256]

--- src/main/java/org/example/betty/common/config/WebConfig.java
+++ src/main/java/org/example/betty/common/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://localhost:5173"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization")

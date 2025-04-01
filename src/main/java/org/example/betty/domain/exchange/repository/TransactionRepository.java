ec4e88de8b3dfbcbbe174a024e70b7ce7052ba5b
이주호 <gernica10@gmail.com>
2025-04-01 09:17:49 +0900

    feat: 트랜잭션 상태 변경 메서드 및 조회 로직 변경 [S12P21A609-189]

--- src/main/java/org/example/betty/domain/exchange/repository/TransactionRepository.java
+++ src/main/java/org/example/betty/domain/exchange/repository/TransactionRepository.java
@@ -2,10 +2,11 @@ package org.example.betty.domain.exchange.repository;
 
 import org.example.betty.domain.exchange.entity.Transaction;
 import org.example.betty.domain.exchange.enums.TransactionStatus;
+import org.example.betty.domain.wallet.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByTransactionStatus(TransactionStatus transactionStatus);
+    List<Transaction> findByWallet(Wallet wallet);
 }

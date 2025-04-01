cbd20e47bde7f1f508184983dcfc997f002731f7
이재훈 <dlwogns1205@naver.com>
2025-04-01 13:19:42 +0900

    test: jenkins 테스트

--- src/main/java/org/example/betty/domain/display/controller/DisplayController.java
+++ src/main/java/org/example/betty/domain/display/controller/DisplayController.java
@@ -28,8 +28,6 @@ public class DisplayController {
 
         List<Display> displayList = displayService.getAllDisplayList(accessToken);
 
-        log.info(displayList.toString());
-
         return ResponseEntity.ok()
                 .body(DisplayResponse.of(displayList));
     }

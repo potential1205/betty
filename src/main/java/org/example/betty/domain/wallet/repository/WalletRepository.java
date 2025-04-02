8b7613477f9cf71523ab8351208070d4dc37a55f
이재훈 <dlwogns1205@naver.com>
2025-04-02 18:33:16 +0900

    feat: [BE] 전광판 그리기 접근 권한 관리 로직 구현 [S12P21A609-331]

--- src/main/java/org/example/betty/domain/wallet/repository/WalletRepository.java
+++ src/main/java/org/example/betty/domain/wallet/repository/WalletRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-    Optional<Wallet> findByWalletAddress(java.lang.String walletAddress);
+    Optional<Wallet> findByWalletAddress(String walletAddress);
     boolean existsByWalletAddress(String walletAddress);
 }

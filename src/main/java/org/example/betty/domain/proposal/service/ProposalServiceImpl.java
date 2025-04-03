781ae2f0722b6159857ce8df73ddd47ec6e44906
이재훈 <dlwogns1205@naver.com>
2025-04-03 17:28:17 +0900

    feat: 사용하지 않는 import 제거 [S12P21A609-344]

--- src/main/java/org/example/betty/domain/proposal/service/ProposalServiceImpl.java
+++ src/main/java/org/example/betty/domain/proposal/service/ProposalServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor

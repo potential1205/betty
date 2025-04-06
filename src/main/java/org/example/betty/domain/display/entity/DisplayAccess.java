2dd945dc9cdeb351e8ce35f77505ac8cb00c147e
이재훈 <dlwogns1205@naver.com>
2025-04-06 16:54:04 +0900

    feat: [BE] 전광판 로직 개편 [S12P21A609-384]

--- src/main/java/org/example/betty/domain/display/entity/DisplayAccess.java
+++ src/main/java/org/example/betty/domain/display/entity/DisplayAccess.java
@@ -1,12 +1,15 @@
 package org.example.betty.domain.display.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.checkerframework.checker.index.qual.GTENegativeOne;
 
 @Entity
 @Getter
@@ -15,6 +18,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DisplayAccess {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull

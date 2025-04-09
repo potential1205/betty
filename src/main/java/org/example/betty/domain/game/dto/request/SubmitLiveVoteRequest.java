3d9063bb771c3dc9cf4b853854d315ff2fb539aa
이성희 <44632544+LeeSeongHui@users.noreply.github.com>
2025-04-09 17:30:38 +0900

    feat : [BE] 라이브 투표 기능 개발 [S12P21A609-458]

--- src/main/java/org/example/betty/domain/game/dto/request/SubmitLiveVoteRequest.java
+++ src/main/java/org/example/betty/domain/game/dto/request/SubmitLiveVoteRequest.java
@@ -1,13 +1,16 @@
 package org.example.betty.domain.game.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubmitLiveVoteRequest {
     private Long gameId;
     private String selectedAnswer;
     private String problemId;
-    private Long homeTeamId;
-    private Long awayTeamId;
-    private Long myTeamId;
 }
